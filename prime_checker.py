2422ca9c942552c4ec3aa6a5c24a9c746f0859dd
vastid87 <39040297+vastid87@users.noreply.github.com>
2020-02-25 11:00:18 +0800

    Update prime_checker.py

--- prime_checker.py
+++ prime_checker.py
@@ -1,13 +1,13 @@
 quit = False
 list1 = []
 while quit == False:
-    num = int(input("\nPlease give me a number.\n(enter 0 to quit)\n"))
+    num = int(input("\nPlease give me a number. I'll check if it's prime.\n(enter 0 to quit)\n"))
     if num == 0:
         break
     elif num == 1:
         print(f"{num} is not prime as it is not divisible by at least 2 numbers.")
     elif num == 2:
-        print(f"{num} is a prime number.")
+        print(f"{num} is a prime number, divisible by itself and 1.")
     else:
         for x in range(2, num - 1):
             if num % x == 0:
@@ -16,6 +16,6 @@ while quit == False:
             print(f"\n{num} is a prime number.")
         for y in list1:
             if num % y == 0:
-                print(f"\n{num} is divisible by {y}.")
+                print(f"\n{num} is divisible by {y}.\n{num} divided by {y} equals {int(num / y)}")
 
     list1.clear()
